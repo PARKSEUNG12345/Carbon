488708b90d5059827a998e62d79f8826fd4e3f45
PARKSEUNG12345 <161485757+PARKSEUNG12345@users.noreply.github.com>
2024-05-30 10:52:31 +0900

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Footer from './Layout/footer'
+import Header from './Layout/header'
+
+
+function App() {
+
+  return (
+    <div>
+    <Header></Header>
+    <Footer></Footer>
+    </div>
+  )
+}
+
+export default App
