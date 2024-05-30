07b06d62d5ae71ade25d0c2c71842cdb068c8458
PARKSEUNG12345 <161485757+PARKSEUNG12345@users.noreply.github.com>
2024-05-30 10:54:23 +0900

    Add files via upload

---
+++ footer.tsx
@@ -0,0 +1,12 @@
+import '../style/FooterStyle.css'
+
+const footer = () => {
+  return (
+    <div className='footer'>
+      <p className="footer-title">Support Bitcoin.org:</p>
+    </div>
+  )
+}
+
+export default footer
+
---
+++ header.tsx
@@ -0,0 +1,11 @@
+import '../style/HeaderStyle.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <p className='header-title'>CAME</p>
+    </div>
+  )
+}
+
+export default Header
