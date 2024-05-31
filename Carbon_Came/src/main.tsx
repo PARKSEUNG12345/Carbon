68a7ccafd267d3569e4cb93d5529363cf992886b
gwang17 <160682609+gwang17@users.noreply.github.com>
2024-05-31 16:42:07 +0900

    test

--- Carbon_Came/src/main.tsx
+++ Carbon_Came/src/main.tsx
@@ -1,7 +1,6 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
 import "@style/index.css";
 import { RouterProvider } from "react-router-dom";
+import { router } from "@/routers/router.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(<RouterProvider router={router} />);
