50d3c3afcd1589b189135420031ea73fd01dfb91
Park <wwwpcs01@gmail.com>
2024-05-31 16:27:58 +0900

    Commit

--- Carbon_Came/src/App.tsx
+++ Carbon_Came/src/App.tsx
@@ -1,4 +1,4 @@
-import "./App.css";
+import "@style/App.css";
 import Header from "@components/Header";
 import MainContent from "@/components/MainContent";
 import Footer from "@/components/Footer";
