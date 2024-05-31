6f947988b45870c7720e3740bba882a3f4e73d4f
Park <wwwpcs01@gmail.com>
2024-05-31 16:19:51 +0900

    Commit

--- Carbon_Came/src/components/MainContent.tsx
+++ Carbon_Came/src/components/MainContent.tsx
@@ -6,25 +6,37 @@ const MainContent = () => {
       <section id="what-is-bitcoin">
         <div className="container">
           <h2>What is Bitcoin?</h2>
-          <p>Bitcoin is an innovative payment network and a new kind of money.</p>
+          <p>모바일로 비트코인 결제를 하실 때에는 "스캔-앤-페이" 두 단계만 거치면 됩니다. 카드를 긁거나, 핀 코드를 입력하거나, 사인을 할 필요가 없습니다. 지불을 받으려면 모바일폰의 비트코인 지갑 앱에서 QR 코드를 꺼내어 상대방이 그 코드를 스캔하거나 두 사용자의 모바일폰을 (NFC 무선 기술을 사용하여) 서로 맞대게 하기만 하면 됩니다.</p>
         </div>
       </section>
       <section id="how-it-works">
         <div className="container">
           <h2>How it Works</h2>
-          <p>Bitcoin uses peer-to-peer technology to operate with no central authority or banks.</p>
+          <p>모바일로 비트코인 결제를 하실 때에는 "스캔-앤-페이" 두 단계만 거치면 됩니다. 카드를 긁거나, 핀 코드를 입력하거나, 사인을 할 필요가 없습니다. 지불을 받으려면 모바일폰의 비트코인 지갑 앱에서 QR 코드를 꺼내어 상대방이 그 코드를 스캔하거나 두 사용자의 모바일폰을 (NFC 무선 기술을 사용하여) 서로 맞대게 하기만 하면 됩니다.</p>
         </div>
       </section>
       <section id="get-started">
         <div className="container">
           <h2>Get Started with Bitcoin</h2>
-          <p>Getting started with Bitcoin is easy. Here are some steps to get you started.</p>
+          <p>모바일로 비트코인 결제를 하실 때에는 "스캔-앤-페이" 두 단계만 거치면 됩니다. 카드를 긁거나, 핀 코드를 입력하거나, 사인을 할 필요가 없습니다. 지불을 받으려면 모바일폰의 비트코인 지갑 앱에서 QR 코드를 꺼내어 상대방이 그 코드를 스캔하거나 두 사용자의 모바일폰을 (NFC 무선 기술을 사용하여) 서로 맞대게 하기만 하면 됩니다.</p>
         </div>
       </section>
       <section id="resources">
         <div className="container">
           <h2>Resources</h2>
-          <p>Here are some resources to help you learn more about Bitcoin.</p>
+          <p>모바일로 비트코인 결제를 하실 때에는 "스캔-앤-페이" 두 단계만 거치면 됩니다. 카드를 긁거나, 핀 코드를 입력하거나, 사인을 할 필요가 없습니다. 지불을 받으려면 모바일폰의 비트코인 지갑 앱에서 QR 코드를 꺼내어 상대방이 그 코드를 스캔하거나 두 사용자의 모바일폰을 (NFC 무선 기술을 사용하여) 서로 맞대게 하기만 하면 됩니다.</p>
+        </div>
+      </section>
+      <section id="resources">
+        <div className="container">
+          <h2>weqr</h2>
+          <p>모바일로 비트코인 결제를 하실 때에는 "스캔-앤-페이" 두 단계만 거치면 됩니다. 카드를 긁거나, 핀 코드를 입력하거나, 사인을 할 필요가 없습니다. 지불을 받으려면 모바일폰의 비트코인 지갑 앱에서 QR 코드를 꺼내어 상대방이 그 코드를 스캔하거나 두 사용자의 모바일폰을 (NFC 무선 기술을 사용하여) 서로 맞대게 하기만 하면 됩니다.</p>
+        </div>
+      </section>
+      <section id="resources">
+        <div className="container">
+          <h2>qwer</h2>
+          <p>모바일로 비트코인 결제를 하실 때에는 "스캔-앤-페이" 두 단계만 거치면 됩니다. 카드를 긁거나, 핀 코드를 입력하거나, 사인을 할 필요가 없습니다. 지불을 받으려면 모바일폰의 비트코인 지갑 앱에서 QR 코드를 꺼내어 상대방이 그 코드를 스캔하거나 두 사용자의 모바일폰을 (NFC 무선 기술을 사용하여) 서로 맞대게 하기만 하면 됩니다.</p>
         </div>
       </section>
     </main>
